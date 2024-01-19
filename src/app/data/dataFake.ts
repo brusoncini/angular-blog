b6566613790d7ccd3d758ab406b3182c979e6319
Bruna Soncini <brusoncini@gmail.com>
2024-01-19 13:09:14 -0300

    conteúdo dos componentes adicionado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo:
+      'https://miro.medium.com/v2/resize:fit:1358/0*KNqK1IFDqENcyGXS',
+    title: 'Novo blog',
+    description: 'Veja o novo blog feito em Angular!',
+  },
+  {
+    id: '2',
+    photo:
+      'https://miro.medium.com/v2/resize:fit:1400/1*moJeTvW97yShLB7URRj5Kg.png',
+    title: 'Typescript',
+    description: 'Veja meu aprendizado em Typescript com esse novo blog! Gostou? Visite meu GitHub que tem mais!',
+  },
+  {
+    id: '3',
+    photo:
+      'https://hermes.dio.me/articles/cover/a4b5221c-5d38-4f82-830e-78a655b87de6.jpg',
+    title: 'JavaScript',
+    description: 'JavaScript é incrível! Com ele, foi possível criar esse blog usando Angular.',
+  },
+  {
+    id: '4',
+    photo:
+      'https://www.thoughtco.com/thmb/094YF3bQBiuV_13n92GFQZWkM7k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/JavaScript-58acbb8a3df78c345bad32c2.jpg',
+    title: 'Praticando programação',
+    description: 'Estudando um pouco todo dia eu irei longe!',
+  },
+];
